fa347fe25c18159df2b92425272e2c9741a7c687
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-09 14:29:58 +0900

    완성

--- 화면구현/index.js
+++ 화면구현/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
-require('events').
+require('dotenv').config();
 
+app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
 
-app.listen()
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/popcat', (req, res) => {
+    res.sendFile(__dirname + '/popcat.html')
+});
+
+app.listen(process.env.PORT, (error) => {
+    if (error) console.log("응답 실패")
+});
