edee8ed230993d91e47ff4d944509293bedab3b8
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-24 23:49:11 +0900

    거의 완성

--- interactiveWeb/mask/mask.js
+++ interactiveWeb/mask/mask.js
@@ -1,11 +1,28 @@
 const c = document.querySelector(".circle");
-let x = 0, y = 0;
+const cursor = document.querySelector(".cursorItem");
+const yes = document.querySelector('.true');
+const no = document.querySelector('.false');
+let x = 0, y = 0, mx = 0, my = 0;
 
 window.onload = () => {
+    yes.addEventListener("mouseover", function() {
+        console.log("hello");
+        cursor.style.transform = `scale(.3)`;
+    })
+
     window.addEventListener("mousemove", e => {
+
         x = e.clientX;
         y = e.clientY;
-        console.log(x, y)
-        c.style.transform = `translate(${x - 50}px, ${y - 50}px)`;
+        // c.style.transform = `translate(${x - 50}px, ${y - 50}px)`;
     });
+    loop();
+}
+
+function loop() {
+    mx += (x - mx) * .09;
+    my += (y - my) * .09;
+    c.style.transform = `translate(${mx}px, ${my}px)`;
+
+    requestAnimationFrame(loop);
 }
