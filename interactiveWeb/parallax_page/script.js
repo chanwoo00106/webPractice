4d195b3ebfff778fe5f7c7c166a15cc127ae2efe
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-25 20:46:15 +0900

    아잇 모르겠다

--- interactiveWeb/parallax_page/script.js
+++ interactiveWeb/parallax_page/script.js
@@ -1,3 +1,10 @@
+const img1 = document.querySelector(".img:nth-child(1)");
+const img2 = document.querySelector(".img:nth-child(2)");
+const img3 = document.querySelector(".img:nth-child(3)");
+const img4 = document.querySelector(".img:nth-child(4)");
+const img5 = document.querySelector(".img:nth-child(5)");
+const img6 = document.querySelector(".img:nth-child(6)");
+
 window.onload = function() {
 
     window.addEventListener('resize', stageResize, false);
@@ -10,8 +17,13 @@ window.onload = function() {
 
 function scrollFunc(e) {
     let scrollTop = document.documentElement.scrollTop;
-    
-    
+
+    img1.style.transform = `translate3d(0, ${scrollTop * .03}px, 0)`;
+    img2.style.transform = `translate3d(0, ${-scrollTop * .03}px, 0)`;
+    img3.style.transform = `translate3d(0, ${-scrollTop * .12}px, 0)`;
+    img4.style.transform = `translate3d(0, ${-scrollTop * .16}px, 0)`;
+    img5.style.transform = `translate3d(0, ${-scrollTop * .22}px, 0)`;
+    img6.style.transform = `translate3d(0, ${-scrollTop * .25}px, 0)`;
 }
 
 function stageResize() {
@@ -19,7 +31,7 @@ function stageResize() {
 }
 
 function loop() {
-  
+    
     window.requestAnimationFrame(loop);
 }
 
