c9dc4a8d4a76a05b53f7184f99fd914f76d4e8b2
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-29 17:08:41 +0900

    new page 완성

--- 화면구현/js/news.js
+++ 화면구현/js/news.js
@@ -1,48 +1,50 @@
 const ul = document.querySelector(".news-list");
 
-const news = new Promise((resolve, reject) => {
-    axios({
-        method: "get",
-        url: `https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=${key}`
-    }).then(data => {
-        resolve(data.data.articles);
-    }).catch(e => {
-        reject(e);
-    })
-});
+async function init(category) {
+    while(ul.firstChild) ul.removeChild(ul.firstChild);
 
+    try {
+        const data = await axios({
+            method: "GET",
+            dataType: 'jsonp',
+            url: `https://newsapi.org/v2/top-headlines?country=kr${category === 'all' ? '' : `&category=${category}`}&apiKey=${key}`
+        });
+    
+        data.data.articles.map(i => {
+            let img = document.createElement("img");
+            img.src = i.urlToImage;
+            let imgUrl = document.createElement('a');
+            imgUrl.href = i.url;
+            imgUrl.append(img);
+    
+            let url = document.createElement('a');
+            url.href = i.url;
+            url.innerText = i.title
+            let title = document.createElement('h1');
+            title.append(url);
+    
+            let p = document.createElement('p');
+            p.innerText = i.description;
+    
+            let date = document.createElement('p');
+            date.innerText = i.publishedAt;
+            date.classList.add('date');
+    
+            let div = document.createElement('div');
+            div.classList.add('text');
+            div.append(title, p, date)
+    
+            let li = document.createElement('li');
+            li.append(imgUrl, div);
+    
+            ul.append(li);
+        });
+    } catch (e) {
+        ul.classList.add('error');
+        ul.innerText = e;
+    }
+}
 
-news.then(data => {
-    data.map(i => {
-        let img = document.createElement("img");
-        img.src = i.urlToImage;
-        let imgUrl = document.createElement('a');
-        imgUrl.href = i.url;
-        imgUrl.append(img);
-
-        let url = document.createElement('a');
-        url.href = i.url;
-        url.innerText = i.title
-        let title = document.createElement('h1');
-        title.append(url);
-
-        let p = document.createElement('p');
-        p.innerText = i.description;
-
-        let date = document.createElement('p');
-        date.innerText = i.publishedAt;
-        date.classList.add('date');
-
-        let div = document.createElement('div');
-        div.classList.add('text');
-        div.append(title, p, date)
-
-        let li = document.createElement('li');
-        li.append(imgUrl, div);
-
-        ul.append(li);
-    })
-}).catch(e => {
-    ul.classList.add('error');
-    ul.innerText = e;
-})
+window.onload = function() {
+    init('all');
+}
