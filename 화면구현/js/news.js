9dca08bd29416c9fbec81ca0597efe9d83693802
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-30 16:58:36 +0900

    일단 커밋

--- 화면구현/js/news.js
+++ 화면구현/js/news.js
@@ -1,7 +1,7 @@
 const ul = document.querySelector(".news-list");
 
 async function init(category) {
-    while(ul.firstChild) ul.removeChild(ul.firstChild);
+    while (ul.firstChild) ul.removeChild(ul.firstChild);
 
     try {
         const data = await axios({
@@ -9,7 +9,7 @@ async function init(category) {
             dataType: 'jsonp',
             url: `https://newsapi.org/v2/top-headlines?country=kr${category === 'all' ? '' : `&category=${category}`}&apiKey=${key}`
         });
-    
+
         data.data.articles.map(i => {
             let li = document.createElement('li');
             if (i.url.indexOf('https://news.google.com/') === -1) {
@@ -20,26 +20,26 @@ async function init(category) {
                 imgUrl.append(img);
                 li.append(imgUrl);
             }
-    
+
             let url = document.createElement('a');
             url.href = i.url;
             url.innerText = i.title
             let title = document.createElement('h1');
             title.append(url);
-    
+
             let p = document.createElement('p');
             p.innerText = i.description;
-    
+
             let date = document.createElement('p');
             date.innerText = i.publishedAt;
             date.classList.add('date');
-    
+
             let div = document.createElement('div');
             div.classList.add('text');
             div.append(title, p, date)
 
             li.append(div);
-    
+
             ul.append(li);
         });
     } catch (e) {
@@ -48,6 +48,6 @@ async function init(category) {
     }
 }
 
-window.onload = function() {
+window.onload = function () {
     init('all');
 }
